cfdf15cf7943e576802e04cee26c287c6fc11a7d
Tent-Dev <bo.chutipas@gmail.com>
2022-01-10 14:10:51 +0700

    Test Compile & Create Sol

---
+++ simple_contract/contracts/Token.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+contract Token {
+    mapping (address => uint256) public balanceOf;
+
+    constructor(uint256 initialSupply) public {
+        balanceOf[msg.sender] = initialSupply;
+    }
+
+    function transfer(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the sender has enough
+        require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        return true;
+    }
+}
