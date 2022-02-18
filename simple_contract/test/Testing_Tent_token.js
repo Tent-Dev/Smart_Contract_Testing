b49ad79740412c803bb0d7d63ecd0c8e821374e1
Tent-Dev <bo.chutipas@gmail.com>
2022-02-18 15:19:24 +0700

    Update Testing Smart Contract

--- simple_contract/test/Testing_Tent_token.js
+++ simple_contract/test/Testing_Tent_token.js
@@ -2,8 +2,8 @@ const TentToken = artifacts.require("Tent_Token");
 
 contract("Tent_Token testing", async accounts => {
   it("should put 100,000,000 Tent_Token in the first account", async () => {
-    const instance = await TentToken.deployed();
-    const balance = await instance.balanceOf.call(accounts[0]);
+    const tokenDeployed = await TentToken.deployed();
+    const balance = await tokenDeployed.balanceOf.call(accounts[0]);
     const checkAmount = web3.utils.toWei('100000000', 'ether');
     assert.equal(balance.valueOf(), checkAmount);
   });
@@ -12,25 +12,34 @@ contract("Tent_Token testing", async accounts => {
     // Get initial balances of first and second account.
     const account_one = accounts[0];
     const account_two = accounts[1];
+    const amount = web3.utils.toWei('10', 'ether');; //amount of token want to send
+    const tokenDeployed = await TentToken.deployed();
 
-    const amount = 10;
+    console.log('Send '+ amount + ' Token (convert from wei is: ' + web3.utils.fromWei(amount, "ether") + ' Token)');
 
-    const instance = await TentToken.deployed();
-    const meta = instance;
+    //Before transfer
+    let balance = await tokenDeployed.balanceOf.call(account_one);
+    const account_one_starting_balance = Number(balance);
+    console.log('Sender Balance before transfer: '+ account_one_starting_balance);
 
-    const balance = await meta.balanceOf.call(account_one);
-    const account_one_starting_balance = balance.toNumber();
+    balance = await tokenDeployed.balanceOf.call(account_two);
+    const account_two_starting_balance = Number(balance);
+    console.log('Receiver Balance before transfer: '+ account_two_starting_balance);
 
-    balance = await meta.balanceOf.call(account_two);
-    const account_two_starting_balance = balance.toNumber();
-    await meta.sendCoin(account_two, amount, { from: account_one });
+    //Transfer
+    await tokenDeployed.transfer(account_two, amount, { from: account_one });
+    
 
-    balance = await meta.balanceOf.call(account_one);
-    const account_one_ending_balance = balance.toNumber();
+    //After transfer
+    balance = await tokenDeployed.balanceOf.call(account_one);
+    const account_one_ending_balance = Number(balance);
+    console.log('Sender Balance after transfer: '+ account_one_ending_balance);
 
-    balance = await meta.balanceOf.call(account_two);
-    const account_two_ending_balance = balance.toNumber();
+    balance = await tokenDeployed.balanceOf.call(account_two);
+    const account_two_ending_balance = Number(balance);
+    console.log('Receiver Balance after transfer: '+ account_two_ending_balance);
 
+    //check before & after balance
     assert.equal(
       account_one_ending_balance,
       account_one_starting_balance - amount,
